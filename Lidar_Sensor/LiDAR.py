b898d39547af9f2e9101c8d2a1aa98f999036b01
Hyeon Lee <hyunlee52852@gmail.com>
2023-07-29 18:59:13 +0900

    Modify Client code, Lidar comms test

--- Lidar_Sensor/LiDAR.py
+++ Lidar_Sensor/LiDAR.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*
 import serial
 import time
+import socket
+from _thread import *
 
-ser = serial.Serial("/dev/serial0", 115200)
+################ Module의 기본 설정 데이터들 ################
 
+ser = serial.Serial("/dev/serial0", 115200) # Module의 통신 포트
+
+HOST = '127.0.0.1' # Main server의 주소
+PORT = 9999 # Main server과 연결할 포트
+MODULENO = 0 ## 모듈 번호에 알맞게 바꾸기
+
+################################ Main Comms ##################################
+# 메인 서버와 통신을 시도한다
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+client_socket.send(f'{MODULENO}'.encode()) ## 통신이 성사되면 모듈 번호를 보낸다
+
+print (f'>> Module {MODULENO} Connected!')
+
+def send_data(data): # data는 string type으로 보내자!!!!
+    client_socket.send(f'{MODULENO}{data}'.encode())
+
+############################### Module Code #################################
 
 def getTFminiData():
     while True:
@@ -18,6 +39,7 @@ def getTFminiData():
             if recv[0] == 0x59 and recv[1] == 0x59:     #python3
                 distance = recv[2] + recv[3] * 256
                 strength = recv[4] + recv[5] * 256
+                send_data(distance) # 데이터 보내기
                 print('(', distance, ',', strength, ')')
                 ser.reset_input_buffer()
 
@@ -31,9 +53,9 @@ if __name__ == '__main__':
     try:
         if ser.is_open == False:
             ser.open()
-            #print(syncdata())
         getTFminiData()
     except KeyboardInterrupt:   # Ctrl+C
         print('exit')
         if ser != None:
             ser.close()
+        client_socket.close()
--- Linux_Data_Comms/client.py
+++ Linux_Data_Comms/client.py
@@ -3,12 +3,18 @@ from _thread import *
 
 HOST = '127.0.0.1'
 PORT = 9999
+MODULENO = 0 ## 모듈 번호에 알맞게 바꾸기
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
+client_socket.send(f'{MODULENO}'.encode()) ## 통신이 성사되면 모듈 번호를 보낸다
+
+
 # 서버로부터 메세지를 받는 메소드
 # 스레드로 구동 시켜, 메세지를 보내는 코드와 별개로 작동하도록 처리
+
+"""
 def recv_data(client_socket) :
     while True :
         data = client_socket.recv(1024)
@@ -17,8 +23,12 @@ def recv_data(client_socket) :
 
 
 start_new_thread(recv_data, (client_socket,))
-print ('>> Connect Server')
+"""
+# Client가 Main server에서 데이터를 받을 일이 있으면 사용
 
+print (f'>> Module {MODULENO} Connected!')
+
+# 보낼 메세지를 여기에 쓰면 된다.
 while True:
     message = input('')
     if message == 'quit':
